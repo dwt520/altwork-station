473347523df30a223b71e562b5573e0b3f61f051
dwt520 <18397969775@163.com>
2018-11-17 15:13:05 +0800

    完成两个图表

---
+++ letao/public/lt-back2/js/index.js
@@ -0,0 +1,78 @@
+$(function () {
+    // 基于准备好的dom，初始化echarts实例
+    var echarts_left = echarts.init(document.querySelector('.echarts_left'));
+
+    // 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: '2018年注册人数'
+        },
+        tooltip: {},
+        legend: {
+            data: ['人数', '销量']
+        },
+        xAxis: {
+            data: ["1月", "2月", "3月", "4月", "5月", "6月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [500, 200, 360, 1000, 610, 520]
+        }, {
+            name: '销量',
+            type: 'bar',
+            data: [400, 600, 560, 800, 410, 820]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    echarts_left.setOption(option1);
+});
+$(function () {
+    var echarts_right = echarts.init(document.querySelector('.echarts_right'));
+
+    // 指定图表的配置项和数据
+    var option2= {
+        title: {
+            text: '热门品牌销售',
+            subtext: '2018年11月',
+            x: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['耐克', '阿迪', '牛逼', '回力', '李宁']
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    { value: 335, name: '耐克' },
+                    { value: 310, name: '阿迪' },
+                    { value: 234, name: '牛逼' },
+                    { value: 135, name: '回力' },
+                    { value: 1548, name: '李宁' }
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    echarts_right.setOption(option2);
+})
