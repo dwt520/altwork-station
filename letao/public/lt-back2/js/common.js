88c26659c76d6bf81ec1d7450c1dd109a7096a05
dwt520 <18397969775@163.com>
2018-11-17 14:33:27 +0800

    全局功能退出功能完成

--- letao/public/lt-back2/js/common.js
+++ letao/public/lt-back2/js/common.js
@@ -1,3 +1,4 @@
+
 // 进度条
 // NProgress.start();
 
@@ -19,3 +20,36 @@ $( document ).ajaxStop(function() {
   NProgress.done();
 },500)
 });
+$(function(){
+    // 公共的功能
+    // 1.导航点击切换
+      $('.lt_aside .category').click(function(){
+        // 让下一个兄弟元素切换显示模式
+        $(this).next().stop().slideToggle();
+      });
+      // 2.左侧菜单列表切换功能
+      $(".lt_topbar .icon_left").click(function(){
+        $('.lt_aside').toggleClass("hidemenu");
+        $('.lt_main').toggleClass("hidemenu");
+        $('.lt_topbar').toggleClass("hidemenu");
+      });
+      // 3退出功能
+      $('.lt_topbar .icon_right').click(function(){
+        // 点击按钮，显示模态框
+        $('#logoutModal').modal("show");//hide隐藏模态框
+      });
+      $('#logoutBtn').click(function(){
+        // 发送请求，让后台销毁当前用户的登录状态
+        $.ajax({
+          type:'get',
+          url:'/employee/employeeLogout',
+          dataType:'json',
+          success:function(info){
+            console.log(info);
+            if(info.success){
+              location.href="login.html"
+            }
+          }
+        })
+      })
+})
